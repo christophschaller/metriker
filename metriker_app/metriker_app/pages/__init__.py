d7b022db25a442ff65c865dfb1e219050813b524
chris <58859629+christophschaller@users.noreply.github.com>
2023-02-17 22:06:04 +0100

    refactor: remove pages build on ft.UserControl

--- metriker_app/metriker_app/pages/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module containing pages for the metriker app.
-"""
-from .data_privacy_page import DataPrivacyPage
-from .user_page import UserPage
-from .challenge_page import ChallengePage
-from .login_page import LoginPage
