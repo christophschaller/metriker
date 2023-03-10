3b6a258708abc828f87d7acccb9d6da5976c1b02
David Buu <78437983+davidbuu@users.noreply.github.com>
2023-03-10 16:26:51 +0100

    challenge class, tabelle und plot

--- metriker_app/metriker_app/views/challenges_view.py
+++ metriker_app/metriker_app/views/challenges_view.py
@@ -5,6 +5,11 @@ from dataclasses import dataclass
 
 import flet as ft
 
+from datetime import date, datetime, timedelta
+import pandas as pd
+import plotly.express as px
+from flet.plotly_chart import PlotlyChart
+
 from .base_view import BaseView
 
 
@@ -23,10 +28,118 @@ CHALLENGES = {
     "bike": Challenge(
         name="bike",
         icon=ft.icons.PEDAL_BIKE,
-        content=ft.Container(content=ft.Text("run")),
+        content=ft.Container(content=ft.Text("Bike")),
     ),
     "run": Challenge(name="run", icon=ft.icons.HIKING, content=ft.Container(content=ft.Text("run"))),
 }
+
+
+class ChallengeContent(ft.Column):
+    def __init__(self, app, sport_type, start_date, end_date, *args, **kwargs):
+        super().__init__(app, *args, **kwargs)
+        self.app = app
+        self.sport_type = sport_type
+        self.start_date = start_date
+        self.end_date = end_date
+
+        self.controls = [
+            ft.Container(self.create_chart()),
+            ft.Container(self.create_table()),
+        ]
+
+    def get_table_rows(self):  # sport="Ride" e.g.
+        user_distance = []
+        for user in self.app.user_handler.values():
+            distance = sum(
+                [
+                    activity.distance
+                    for activity in self.app.activity_handler.get_user_activities(
+                        user.id,
+                        sport_type=self.sport_type,
+                        start_date="2023-01-01 00:02:00",
+                        end_date="2023-01-02 00:001:00",
+                    )
+                ]
+            )
+            user_distance.append((user, distance))
+
+        sorted_user_distance = sorted(user_distance, key=lambda x: x[1], reverse=True)
+
+        rows = []
+        for i, elem in enumerate(sorted_user_distance):
+            rows.append(
+                ft.DataRow(
+                    cells=[
+                        ft.DataColumn(ft.Text(i + 1)),
+                        ft.DataCell(
+                            # ft.Text(elem[0].name),
+                            ft.TextButton(
+                                text=elem[0].name,
+                                data=elem[0].id,
+                                on_click=lambda e: self.app.page.go(f"/user/{e.control.data}"),
+                            )
+                            # on_tap=lambda e: self.app.page.go(f"/user/{elem[0].id}"),
+                        ),  # TODO: find better way to link users
+                        ft.DataCell(ft.Text(elem[1])),
+                    ],
+                ),
+            )
+        return rows
+
+    def get_sport_plot(self, start_date, end_date):
+        start_date = datetime.strptime(str(start_date), "%Y-%m-%d %H:%M:%S")
+        end_date = datetime.strptime(str(end_date), "%Y-%m-%d %H:%M:%S")
+
+        all_activities = []
+        for user in self.app.user_handler.values():
+            month_list = []
+            for month in range(end_date.month - start_date.month + 1):
+                month_list.append(start_date.month + month)
+
+            user_activities = []
+            for month in month_list:
+                new_start_date = datetime(2023, month, 1, 0, 0, 0)
+                new_end_date = datetime(2023, month + 1, 1, 0, 0, 0) - timedelta(seconds=1)
+
+                month_distance = sum(
+                    [
+                        activity.distance
+                        for activity in self.app.activity_handler.get_user_activities(
+                            user.id,
+                            sport_type=self.sport_type,
+                            start_date=new_start_date,
+                            end_date=new_end_date,
+                        )
+                    ]
+                )
+                # month_distance = (month,month_distance)
+                user_activities.append(month_distance)
+            all_activities.append(user_activities)
+            # print("###### user_activities = "+str(user_activities))
+
+        # month_columns = []
+        # usernames_index = []
+        df = pd.DataFrame(all_activities)  # ,columns=month_columns, index=usernames_index)
+        # print(df)
+
+        # sport_graph = PlotlyChart(px.line(df#.transpose()
+        #                                  ))#,template="simple_white", line_shape="spline",labels={'value':'Kilometer', 'index':'Monate'}))#, expand=True)
+        return df  # sport_graph
+
+    def create_chart(self):
+        return PlotlyChart(
+            px.line(self.get_sport_plot("2023-01-01 00:00:05", "2023-03-03 00:00:05").transpose())
+        )  # ,template="simple_white", line_shape="spline",labels={'value':'Kilometer', 'index':'Monate'}))#, expand=True)# self._active_content
+
+    def create_table(self):
+        return ft.DataTable(
+            columns=[
+                ft.DataColumn(ft.Text("Place"), numeric=True),
+                ft.DataColumn(ft.Text("Name")),
+                ft.DataColumn(ft.Text("Distance"), numeric=True),
+            ],
+            rows=self.get_table_rows(),
+        )
 
 
 class ChallengesView(BaseView):
@@ -46,41 +159,28 @@ class ChallengesView(BaseView):
         self.app = app
         self.route = "/challenges"
 
-        self.challenges = CHALLENGES
+        # TODO: find proper place for challenge configuration
+        self.challenges = {
+            "bike": Challenge(
+                name="bike",
+                icon=ft.icons.PEDAL_BIKE,
+                content=ChallengeContent(self.app, "bike", "2023-01-01 00:00:01", "2023-03-03 00:00:05"),
+            ),
+            "run": Challenge(
+                name="run",
+                icon=ft.icons.HIKING,
+                content=ChallengeContent(self.app, "run", "2023-01-01 00:00:01", "2023-03-03 00:00:05"),
+            ),
+        }
         self.nav_bar = self._create_nav_bar()
         self._active_content = ft.Container()
+        self.padding = ft.padding.all(100)
 
         # add controls to frame
         self.extend_controls()
 
     def extend_controls(self) -> None:
-        self.controls.extend(
-            [
-                self.nav_bar,
-                ft.DataTable(
-                    columns=[
-                        ft.DataColumn(ft.Text("Place"), numeric=True),
-                        ft.DataColumn(ft.Text("Name")),
-                        ft.DataColumn(ft.Text("Distance"), numeric=True),
-                    ],
-                    rows=self.get_table_rows("Ride"),
-                ),
-                ft.Column(
-                    # hier eine challenge
-                    controls=[
-                        ft.FilledButton(
-                            text=f"{user.name}",
-                            on_click=lambda e: self.app.page.go(f"/user/{e.control.data}"),
-                            data=user.id,
-                        )
-                        for user in self.app.user_handler.values()
-                    ],
-                    scroll=ft.ScrollMode.AUTO,
-                    expand=True,
-                ),
-                # self._active_content,
-            ]
-        )
+        self.controls.extend([self.nav_bar, self._active_content])
 
     def _create_nav_bar(self) -> ft.NavigationBar:
         """
