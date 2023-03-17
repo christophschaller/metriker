7f237d16c97dc3b98be636c855f54ce513c53021
David Buu <78437983+davidbuu@users.noreply.github.com>
2023-03-17 16:14:56 +0100

    add activity plot on user screen

--- metriker_app/metriker_app/views/user_view.py
+++ metriker_app/metriker_app/views/user_view.py
@@ -1,7 +1,12 @@
 """
 Module holding UserView.
 """
+from datetime import datetime, timedelta
+
 import flet as ft
+import pandas as pd
+import plotly.express as px
+from flet.plotly_chart import PlotlyChart
 
 from database_utils.user_handler import StravaUser
 
@@ -31,7 +36,11 @@ class UserView(BaseView):
 
     def extend_controls(self) -> None:
         self.controls.extend(
-            [ft.Text(self.user.name), ft.Container(self.create_table("2023-01-01 00:00:05", "2023-03-03 00:00:05"))]
+            [
+                ft.Text(self.user.name),
+                ft.Container(self.create_chart("2023-01-01 00:00:05", "2023-03-03 00:00:05", "frame")),
+                ft.Container(self.create_table("2023-01-01 00:00:05", "2023-03-03 00:00:05")),
+            ]
         )
 
     def create_table(self, start_date, end_date):
@@ -65,19 +74,15 @@ class UserView(BaseView):
         """
         user_distance = []
         for user in self.app.user_handler.values():
-            sport_types = (
-                set(
-                    [
-                        activity.type
-                        for activity in self.app.activity_handler.get_user_activities(
-                            user.id,
-                            # sport_type="Ride",
-                            start_date=start_date,
-                            end_date=end_date,
-                        )
-                    ]
-                )
-                # / 1000
+            sport_types = set(
+                [
+                    activity.type
+                    for activity in self.app.activity_handler.get_user_activities(
+                        user.id,
+                        start_date=start_date,
+                        end_date=end_date,
+                    )
+                ]
             )
 
             for sport_type in sport_types:
@@ -97,7 +102,7 @@ class UserView(BaseView):
                 )
 
                 user_distance.append((sport_type, distance))
-            print(user_distance)
+            # print(user_distance)
 
         sorted_user_distance = sorted(user_distance, key=lambda x: x[1], reverse=True)
 
@@ -117,3 +122,132 @@ class UserView(BaseView):
                 ),
             )
         return rows
+
+    def get_sport_plot(self, start_date, end_date, frame):
+        """returns all values for a plot with plotly in a pd dataframe format
+
+        Args:
+            start_date (str in format "%Y-%m-%d %H:%M:%S"): start_date
+            end_date (str in format "%Y-%m-%d %H:%M:%S"): end_date
+            frame(str): month or empty changes the detail leve of the plot (weeks or months)
+
+        Returns:
+            get_sport_plot(pandas dataframe): dataframe for plotly chart
+        """
+        start_date = datetime.strptime(str(start_date), "%Y-%m-%d %H:%M:%S")
+        end_date = datetime.strptime(str(end_date), "%Y-%m-%d %H:%M:%S")
+
+        """# Month steps
+        if frame == "month":
+            all_activities = []
+            for user in self.app.user_handler.values():
+                month_list = []
+                for month in range(end_date.month - start_date.month + 1):
+                    month_list.append(start_date.month + month)
+
+                user_activities = []
+                for month in month_list:
+                    new_start_date = datetime(2023, month, 1, 0, 0, 0)
+                    new_end_date = datetime(2023, month + 1, 1, 0, 0, 0) - timedelta(seconds=1)
+
+                    month_distance = (
+                        sum(
+                            [
+                                activity.distance
+                                for activity in self.app.activity_handler.get_user_activities(
+                                    user.id,
+                                    sport_type="Ride",
+                                    start_date=new_start_date,
+                                    end_date=new_end_date,
+                                )
+                            ]
+                        )
+                        / 1000
+                    )
+                    user_activities.append(month_distance)
+                all_activities.append(user_activities)
+
+            month_columns = []
+            for i in range(0, end_date.month - start_date.month + 1):
+                next_month = start_date + relativedelta(months=i)
+                next_month_name = next_month.strftime("%B")
+                month_columns.append(next_month_name)
+
+            sport_type_index = [user.name for user in self.app.user_handler.values()]
+            dataframe = pd.DataFrame(all_activities, columns=month_columns, index=sport_type_index)
+
+        # Week steps
+        else:"""
+        all_activities = []
+        for user in self.app.user_handler.values():
+            week_columns = []
+
+            sport_types = list(
+                set(
+                    [
+                        activity.type
+                        for activity in self.app.activity_handler.get_user_activities(
+                            user.id,
+                            start_date=start_date,
+                            end_date=end_date,
+                        )
+                    ]
+                )
+            )
+
+            # Determine the first and last day of the first week
+            first_day = start_date - timedelta(days=start_date.weekday())
+            last_day = first_day + timedelta(days=7) - timedelta(seconds=1)
+
+            # Loop through each week and print the first day, last day, and ISO week number
+            while first_day.date() <= end_date.date():
+                year, week_num, day_num = first_day.isocalendar()
+                week_columns.append(f"woche {year}-{week_num}")
+                new_start_date = first_day
+                new_end_date = last_day
+
+                user_week_activities = []
+                for sport_type in sport_types:
+                    week_distance = (
+                        sum(
+                            [
+                                activity.distance
+                                for activity in self.app.activity_handler.get_user_activities(
+                                    user.id,
+                                    sport_type=sport_type,
+                                    start_date=new_start_date,
+                                    end_date=new_end_date,
+                                )
+                            ]
+                        )
+                        / 1000
+                    )
+                    user_week_activities.append(week_distance)
+
+                # Increment to the next week
+                first_day = last_day + timedelta(days=1)
+                last_day = first_day + timedelta(days=6)
+
+                all_activities.append(user_week_activities)
+
+            dataframe = pd.DataFrame(all_activities, index=week_columns, columns=sport_types).transpose()
+
+        return dataframe
+
+    def create_chart(self, start_date, end_date, frame):
+        """uses the function get_sport_plot to generate a dataframe and creates a PlotlyChart with that
+        Args:
+            start_date (str in format "%Y-%m-%d %H:%M:%S"): start_date
+            end_date (str in format "%Y-%m-%d %H:%M:%S"): end_date
+
+        Returns:
+            (str in format "%Y-%m-%d %H:%M:%S")(PlotlyChart): chart with values
+        """
+        return PlotlyChart(
+            px.line(
+                self.get_sport_plot(start_date, end_date, frame).transpose(),
+                template="simple_white",
+                line_shape="spline",
+                labels={"value": "Kilometer", "index": "Monate"},
+            )
+        )
