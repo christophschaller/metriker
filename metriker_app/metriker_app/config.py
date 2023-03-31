f98f6bf29fadead61a9511f35920539dc8c365e7
chris <58859629+christophschaller@users.noreply.github.com>
2023-03-31 11:43:44 +0200

    fix: secrets should be SecretStr

--- metriker_app/metriker_app/config.py
+++ metriker_app/metriker_app/config.py
@@ -3,7 +3,7 @@ Config for the flet webapp.
 The config is read from env vars, .env files and default values in this order.
 """
 # pylint:disable=duplicate-code
-from pydantic import AnyUrl, BaseSettings
+from pydantic import AnyUrl, SecretStr, BaseSettings
 
 
 class Settings(BaseSettings):
@@ -21,9 +21,14 @@ class Settings(BaseSettings):
         env_prefix = "METRIKER_"
 
     ENVIRONMENT: str
+    LOGGING_CONFIG_PATH: str = "../../logging.ini"
+    SENTRY_DSN: AnyUrl = None
+
+    STRAVA_SERVICE_URL: AnyUrl
+    STRAVA_SERVICE_TIMEOUT: int = 60
 
     STRAVA_CLIENT_ID = str
-    STRAVA_CLIENT_SECRET = str
+    STRAVA_CLIENT_SECRET = SecretStr
     STRAVA_AUTH_ENDPOINT = AnyUrl
     STRAVA_TOKEN_ENDPOINT = AnyUrl
     STRAVA_REDIRECT_URL = AnyUrl
@@ -31,18 +36,12 @@ class Settings(BaseSettings):
     STRAVA_USER_SCOPES = str
 
     DB_USER = str
-    DB_PASS = str
+    DB_PASS = SecretStr
     DB_HOST = AnyUrl
     DB_PORT = int
     DB_NAME = str
 
-    STRAVA_SERVICE_URL: AnyUrl
-    STRAVA_SERVICE_TIMEOUT: int = 60
-
-    SECRET_KEY = str
-
-    LOGGING_CONFIG_PATH: str = "../../logging.ini"
-    SENTRY_DSN: AnyUrl = None
+    SECRET_KEY = SecretStr
 
 
 settings = Settings()
