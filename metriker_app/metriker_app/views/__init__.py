a942125ea353c4256aa2851d114f180d0bdb757e
chris <58859629+christophschaller@users.noreply.github.com>
2023-04-03 09:00:08 +0200

    refactor: sort imports

--- metriker_app/metriker_app/views/__init__.py
+++ metriker_app/metriker_app/views/__init__.py
@@ -1,4 +1,7 @@
 """The vies package holds all views of the Metriker App."""
 from .challenges_view import ChallengesView
-from .login_view import LoginView
 from .data_privacy_view import DataPrivacyView
+from .login_view import LoginView
+from .user_view import UserView
+
+__all__ = ["UserView", "ChallengesView", "LoginView", "DataPrivacyView"]
